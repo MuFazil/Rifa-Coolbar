8a704a6f6dc0daf8cec184710ae3d18373a30ab9
Fazil <mhdfazil61@gmail.com>
2022-08-04 00:41:17 +0530

    Final

---
+++ app.js
@@ -0,0 +1,12 @@
+$('#carousel-1').carousel({
+    interval: 4000,
+    wrap: true,
+    keyboard: true
+});
+
+
+$('#carousel-2').carousel({
+    interval: 6000,
+    wrap: true,
+    keyboard: true
+});
